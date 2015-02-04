ae665e0225d63c983448d7a04683b5ce2fbc422b
Sherry Zhou <5643f56c@opayq.com>
2015-02-03 17:45:17 -0800

    Remove empty directory from require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require underscore
 //= require backbone
 //= require instacation
-//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
