ef3592d4c17416598b9f0fa77ecf007a3a177207
Sherry Zhou <5643f56c@opayq.com>
2015-02-11 12:48:31 -0800

    Redid text resizing function + added footer resize

--- app/assets/javascripts/instacation.js
+++ app/assets/javascripts/instacation.js
@@ -21,12 +21,38 @@ window.Instacation = {
     var $root = $('#main');
     this.currentUserId = $root.data('current-user');
     new Instacation.Routers.UserData({$rootEl: $root});
+    this.addEvent(window, "resize", this.changeHeightForFooter);
+    this.addEvent(window, "resize", this.resize);
     Backbone.history.start();
   },
 
+  // Took so long! But now all tax with the class "fitable-text" will be resized
   resize: function () {
-    while( $('.fitable-text').height() > $('.fit-container').height() ) {
-        $('.fitable-text').css('font-size', (parseInt($('.fitable-text').css('font-size')) - 1) + "px" );
+    $('.fitable-text').each(function (index, textArea) {
+      while( $(textArea).height() > $($('.fit-container')[index]).height() ) {
+        $(textArea).css('font-size', (parseInt($(textArea).css('font-size')) - 1) + "px" );
+      }
+    });
+  },
+
+  changeHeightForFooter: function () {
+    var docHeight = $(window).height();
+     var footerHeight = $('footer').height();
+     var footerTop = $('footer').position().top + footerHeight;
+
+     if (footerTop < docHeight) {
+      $('footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+     }
+  },
+
+  addEvent: function (elem, type, eventHandle) {
+    if (elem === null || typeof(elem) == 'undefined') return;
+    if ( elem.addEventListener ) {
+        elem.addEventListener( type, eventHandle, false );
+    } else if ( elem.attachEvent ) {
+        elem.attachEvent( "on" + type, eventHandle );
+    } else {
+        elem["on"+type]=eventHandle;
     }
   },
 };
